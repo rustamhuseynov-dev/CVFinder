d95c4fdf4fec27fea1376d3d69e3b4c392a371a0
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-22 19:43:53 +0400

    Create TokenDTO.java

---
+++ src/main/java/com/rustam/CVFinder/dto/TokenDTO.java
@@ -0,0 +1,15 @@
+package com.rustam.CVFinder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenDTO {
+
+    private TokenPair tokenPair;
+}
