208faedf33990b4e50e4d6ac3f633c3b129de596
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-22 13:49:03 +0400

    Create CvFinderApplication.java

---
+++ src/main/java/com/rustam/CVFinder/CvFinderApplication.java
@@ -0,0 +1,13 @@
+package com.rustam.CVFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvFinderApplication.class, args);
+	}
+
+}
