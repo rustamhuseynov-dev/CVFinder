f8eacc036ce120bc44ffd3e91e5bd83059728f5b
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-22 19:43:57 +0400

    Create TokenPair.java

---
+++ src/main/java/com/rustam/CVFinder/dto/TokenPair.java
@@ -0,0 +1,16 @@
+package com.rustam.CVFinder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenPair {
+
+    private String accessToken;
+    private String refreshToken;
+}
