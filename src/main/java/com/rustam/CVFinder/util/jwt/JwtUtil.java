154bf4053ab26e8d89eb7c25bfa22559d3804224
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-22 19:44:31 +0400

    Update JwtUtil.java

--- src/main/java/com/rustam/CVFinder/util/jwt/JwtUtil.java
+++ src/main/java/com/rustam/CVFinder/util/jwt/JwtUtil.java
@@ -7,10 +7,11 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
-@Component
+@Service
 public class JwtUtil {
 
     @Value("${spring.application.jwt.secret-key}")
