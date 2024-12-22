0b5a22ea0b79a032f415b05ed92ef6ef8b01c243
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-22 19:43:39 +0400

    Update AuthRepository.java

--- src/main/java/com/rustam/CVFinder/dao/repository/AuthRepository.java
+++ src/main/java/com/rustam/CVFinder/dao/repository/AuthRepository.java
@@ -1,11 +1,18 @@
 package com.rustam.CVFinder.dao.repository;
 
 import com.rustam.CVFinder.dao.entity.BaseUser;
+import com.rustam.CVFinder.dao.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AuthRepository extends JpaRepository<BaseUser, UUID> {
+
+    @Query(value = "SELECT * FROM base_users WHERE user_type = 'USER' AND username = :username", nativeQuery = true)
+    Optional<User> findByUsername(String username);
 }
