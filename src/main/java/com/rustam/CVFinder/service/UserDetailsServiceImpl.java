7f7ef5d65e8f0497623f1558fec6c7b891f43a39
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-22 19:44:28 +0400

    Update UserDetailsServiceImpl.java

--- src/main/java/com/rustam/CVFinder/service/UserDetailsServiceImpl.java
+++ src/main/java/com/rustam/CVFinder/service/UserDetailsServiceImpl.java
@@ -1,14 +1,28 @@
 package com.rustam.CVFinder.service;
 
+import com.rustam.CVFinder.dao.entity.BaseUser;
+import com.rustam.CVFinder.dao.entity.User;
+import com.rustam.CVFinder.dao.repository.AuthRepository;
+import com.rustam.CVFinder.exception.custom.BaseUserNotFoundException;
+import com.rustam.CVFinder.util.UtilService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AuthRepository authRepository;
+    private final UtilService utilService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        BaseUser user = authRepository.findById(utilService.convertToUUID(username))
+                .orElseThrow(() -> new BaseUserNotFoundException("User Not Found with username: " + username));
+
+        return new CustomUserDetails(user);
     }
 }
