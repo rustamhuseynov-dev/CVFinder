2ff71ed45bf7fd73708d7f7c076f917ad276f05e
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-24 18:58:16 +0400

    Update RefreshTokenRequest.java

--- src/main/java/com/rustam/CVFinder/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/rustam/CVFinder/dto/request/RefreshTokenRequest.java
@@ -1,5 +1,6 @@
 package com.rustam.CVFinder.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RefreshTokenRequest {
 
+    @NotBlank
     private String refreshToken;
 }
