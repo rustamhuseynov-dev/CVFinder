d25074c54fd10fac2717ce2a6c9a68595c393022
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-24 18:58:08 +0400

    Update AuthRequest.java

--- src/main/java/com/rustam/CVFinder/dto/request/AuthRequest.java
+++ src/main/java/com/rustam/CVFinder/dto/request/AuthRequest.java
@@ -1,5 +1,7 @@
 package com.rustam.CVFinder.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +13,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class AuthRequest {
 
+    @NotBlank(message = "The name column cannot be empty.")
     private String name;
+    @Pattern(regexp = "[a-z]+@[a-z]+\\.[a-z]{2,4}", message = "Enter your email address.")
     private String email;
+    @NotBlank(message = "The username column cannot be empty.")
     private String username;
+    @NotBlank(message = "The password column cannot be empty.")
     private String password;
+    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{4}", message = "The phone number must be in the format (000)-000-0000.")
     private String phone;
 
 }
