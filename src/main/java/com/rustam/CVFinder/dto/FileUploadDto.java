e55654beac023b45cd59919bad0858c655ca7ce3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 10:07:06 +0400

    Create FileUploadDto.java

---
+++ src/main/java/com/rustam/CVFinder/dto/FileUploadDto.java
@@ -0,0 +1,16 @@
+package com.rustam.CVFinder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FileUploadDto {
+
+    private MultipartFile file;
+}
