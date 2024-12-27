8750b8c58c461d632e4f7a07158caf876654cf01
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 11:33:57 +0400

    Update FileService.java

--- src/main/java/com/rustam/CVFinder/service/FileService.java
+++ src/main/java/com/rustam/CVFinder/service/FileService.java
@@ -1,6 +1,8 @@
 package com.rustam.CVFinder.service;
 
 import com.rustam.CVFinder.dto.FileUploadDto;
+import com.rustam.CVFinder.dto.request.CvFilterRequest;
+import com.rustam.CVFinder.util.FileProcessor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,11 +11,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
+    private final FileProcessor fileProcessor;
 
     public String fileUpload(FileUploadDto fileUploadDto) {
         MultipartFile file = fileUploadDto.getFile();
@@ -26,5 +31,23 @@ public class FileService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<String> filterCvsBySkill(CvFilterRequest cvFilterRequest) {
+        List<String> filteredCvs = new ArrayList<>();
+
+        for (String filePath : cvFilterRequest.getFilePaths()) {
+            String text = fileProcessor.extractTextFromPdf(filePath);
+
+            if (hasRequiredSkill(text, cvFilterRequest.getRequiredSkill())) {
+                filteredCvs.add(filePath);
+            }
+        }
+
+        return filteredCvs;
+    }
+
+    private boolean hasRequiredSkill(String text, String requiredSkill) {
+        return text.contains("Skills") && text.contains(requiredSkill);
     }
 }
