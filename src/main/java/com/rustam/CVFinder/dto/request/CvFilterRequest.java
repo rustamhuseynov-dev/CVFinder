51b4788073fc9b621a664e310567f71165cd4909
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 11:33:44 +0400

    Create CvFilterRequest.java

---
+++ src/main/java/com/rustam/CVFinder/dto/request/CvFilterRequest.java
@@ -0,0 +1,17 @@
+package com.rustam.CVFinder.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CvFilterRequest {
+    private List<String> filePaths;
+    private String requiredSkill;
+}
