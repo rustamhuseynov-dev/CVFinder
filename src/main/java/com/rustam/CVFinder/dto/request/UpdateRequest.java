08a12f39b82a98b351de083f19bb9f9a5886cd62
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-02 17:03:34 +0400

    Update UpdateRequest.java

--- src/main/java/com/rustam/CVFinder/dto/request/UpdateRequest.java
+++ src/main/java/com/rustam/CVFinder/dto/request/UpdateRequest.java
@@ -1,6 +1,7 @@
 package com.rustam.CVFinder.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +15,8 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class UpdateRequest {
-    @NotBlank(message = "The id column cannot be empty.")
+
+    @NotNull(message = "The id column cannot be empty.")
     private UUID id;
     @NotBlank(message = "The name column cannot be empty.")
     private String name;
